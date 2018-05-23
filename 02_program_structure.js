7b1288f4ea9424b7928bedff2085cbc53b0609fc
David Delgado <ddelgad5@hawk.iit.edu>
2018-05-22 20:43:01 -0500

    Add code for looping a triangle.

--- 02_program_structure.js
+++ 02_program_structure.js
@@ -10,3 +10,10 @@ console.log(Math.min(30,21,9) + 200);
 //cannot perform prompt exercise on node.js
 
 if (1 + 1 == 2) console.log("It's true");
+
+console.log("\nLooping a triangle\n");
+let hash = "#";
+for (let rows=0; rows < 7; rows++) {
+  console.log(hash+"\n");
+  hash = hash + "#";
+}

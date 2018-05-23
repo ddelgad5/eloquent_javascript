c1c5b6d68e26d6826e72b88bdb2a421108063f16
David Delgado <ddelgad5@hawk.iit.edu>
2018-05-22 20:33:54 -0500

    Add log.

--- 02_program_structure.js
+++ 02_program_structure.js
@@ -1,7 +1,4 @@
 //Chapter 2
-//prompt functionality for node.js
-var prompt = require('prompt');
-prompt.start();
 
 let x = 30;
 console.log("The value of x is :", x);
@@ -11,3 +8,5 @@ console.log(Math.max(30,-9,099,91));
 console.log(Math.min(30,21,9) + 200);
 
 //cannot perform prompt exercise on node.js
+
+if (1 + 1 == 2) console.log("It's true");
